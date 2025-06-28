1c969e8521dad4a580f067f2a23e2a12a5e4c75c
ireneiroha <ireneiroha@gmail.com>
2025-06-28 21:29:05 +0100

    Initial commit: HealTrack backend structure

---
+++ HealTracker/src/main/java/com/HealTrackApplication/HealTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.HealTrackApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealTrackerApplication.class, args);
+	}
+
+}
---
+++ HealTracker/src/test/java/com/HealTrackApplication/HealTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HealTrackApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
